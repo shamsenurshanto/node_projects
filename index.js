374e5db8342da38cca2ecb370fae804c2d230c45
shamsenurshanto <180104128@aust.edu>
2023-02-11 00:51:22 +0600

    2nsd

--- index.js
+++ index.js
@@ -5,9 +5,3 @@ const car = {
     name : "fff"
 }
 module.exports = car;
-
-
-
-const cd1 = require('./index');
-console.log("alhamduliilah happpy ");
-console.log(" ",cd1.brand);
---
+++ mod.js
@@ -0,0 +1,3 @@
+const cd1 = require('./index');
+console.log("alhamduliilah happpy ");
+console.log(" ",cd1.brand);
